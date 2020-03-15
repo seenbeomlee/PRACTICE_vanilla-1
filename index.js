9f192589c96d02e2983ab6f3791bc2625e749aa1
Seenbeom Lee <mail@seenblee.com>
2020-03-15 18:01:52 +0900

    vanilla-1 2.3
    vanilla-1 2.3

---
+++ index.js
@@ -0,0 +1,72 @@
+// const monday = "Mon";
+// const tue = "Tue";
+// const wed = "Wed";
+// const thu = "Thy";
+// const fri = "Fri";
+
+// console.log(monday, tue, wed, thu, fri);
+
+// const days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+// console.log(days_of_week[2]);
+// //
+// const nico_info = {
+//   name :"Nicolas",
+//   age : 33,
+//   gender : "male",
+//   is_handsome : true,
+//   fav_moives: ["Along the gods", "LOTR", "QQ coin"],
+
+//   fav_food: [{name: "kimchi", fatty:false}, {name: "cheese burger", fatty: true}]}
+
+//   console.log(nico_info.fav_food[0]);
+
+//   nico_info.gender = "female"
+
+//   console.log(nico_info.gender);
+// //
+// function sayHello(name, age) {
+//   // console.log(`Hello ${name} you are ${age} years old` );
+//   return `Hello ${name} you are ${age} years old`;
+// }
+
+// const greetNicolas = sayHello("Nicolas", 14)
+
+// console.log(greetNicolas)
+// //
+// const calculator = {
+// plus: function(a, b){
+//   return a + b;
+// },
+// divide: function(a, b){
+//   return a / b;
+// },
+// minus: function(a, b){
+//   return a -b;
+// },
+// mult: function(a, b){
+//   return a * b;
+// },
+// pow: function(a, b){
+//   return a ** b;
+// }
+// }
+
+// var plus = calculator.plus(5, 5)
+
+// var minus = calculator.minus(5, 5)
+
+// var mult = calculator.mult(5, 5)
+
+// var divide = calculator.divide(5, 5)
+
+// var pow = calculator.pow(3, 3)
+
+// console.log(`plus: ${plus}, minus: ${minus}, mult: ${mult}, divide: ${divide}, pow: ${pow}`)
+// //
+
+const title = document.querySelector("#title");
+
+title.innerHTML = "Hi! From JS";
+title.style.color = "red";
+document.title = "I own you now";
