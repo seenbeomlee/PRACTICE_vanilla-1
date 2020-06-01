802a0eee27a0ab2c6f5a9772cc45631c2b0e2509
Seenbeom Lee <mail@seenblee.com>
2020-06-01 22:01:04 +0900

    1.00: practice: practice for standard Vjs
    1.00: practice: practice for standard Vjs

---
+++ challenge/README.md
@@ -0,0 +1,28 @@
+#1 restudy for vanilla javascript => make todo-list with js
+/i will use the repl for restudy & and save here
+#2 Do 2 week chanllenge for practice Vjs
+
+#1.0
+/socket.io => demo => Chat with someone in realtime
+/three.js => we can play games with js / like 'World draw'
+/impactjs.com => ''
+/taotajima.jp => cool webpage with js
+
+es5, es6
+/ECMAscript5 = es5
+/ECMAscript6 = es6
+/it is specification of js / chrome, internet-explore .. receives this with their own method
+
+Vanilla js => js with none library
+
+vanilla-js.com => vanilla js standard-docs
+
+difference between 'let' and 'let' and 'const'
+/
+/
+/
+
+difference between '' and "" and ``
+/
+/
+/
---
+++ challenge/index.js
@@ -0,0 +1,84 @@
+/* this is multi-line-comments in js */
+// this is single-line-comment in js
+
+// alert("Im working!");
+
+/* string
+const wat = "Meme"
+*/
+
+/* boolean
+const wat = true;
+const tat = false;
+*/
+
+/*
+const wat = 55;
+*/
+
+/* float
+const wa = 55.1;
+*/
+
+/* array
+const something = "Something"
+
+const days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", 54, true, "stuff", "lalala", something];
+}
+
+console.log(days_of_week);
+*/
+
+/* object
+const myInfo = {
+    name: "LSB",
+    age: "22",
+    gender: "Male",
+    isHandsome: true,
+    fav_games: ["watchdogs", "control", "LOL"];
+    fav_food: {
+        name: "kimchi",
+        is_hot: true
+    }
+}
+
+console.log(myInfo.name);
+console.log(myInfo)
+
+// console is also an object!! and there is 'log'
+console.log(console);
+*/
+
+/* function
+function say_hello(name, num){
+    console.log('Hello!', name, " you have: ", num);
+}
+
+sayHello("seenbeom lee", 22);
+
+=> console.log(`Hello ${name} you are ${age} years old`)
+
+///
+
+function say_hello(name, num){
+    return 'Hello!', name, " you have: ", num;
+}
+
+const greet_name = sayHello("seenbeom lee, 22")
+
+console.log(greet_name)
+
+///
+const calculator = {
+    plus: function(a, b) {
+        return a + b;
+    }
+}
+
+const plus = calculator.plus(5, 5)
+console.log(plus)
+*/
+
+/* function with string
+
+*/
